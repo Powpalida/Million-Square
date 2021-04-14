3f6b3d82b0d1fe404e119de189ae3174eead766b
6110450995 <saharat.chut@ku.th>
2021-04-14 23:41:11 +0700

    separate login

---
+++ src/main/java/th/ac/ku/MillionSquare/model/Ad_min.java
@@ -0,0 +1,57 @@
+package th.ac.ku.MillionSquare.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    private int id;
+
+    private String name;
+    private String pin;
+    private String tel;
+
+    public Admin(int id, String name, String pin,String tel) {
+        this.id = id;
+        this.name = name;
+        this.pin = pin;
+        this.tel = tel;
+    }
+    public Admin(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
+
